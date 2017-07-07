73d24848ea8b7d412a38efa2f47984e4001e0d34
rptine <ryanptine@gmail.com>
2017-07-07 17:55:12 -0400

    Added helper functions(binstringToBitList,padBits,bitsToString,and bitsToChar) and implemented intToString() so that base 10 integers can be converted back to strings based on their ASCII values

--- PasswordEncrypt.py
+++ PasswordEncrypt.py
@@ -15,10 +15,36 @@ def bitListToInt(bitList):
     return int(''.join([('0','1')[int(e)] for e in bitList]),2)
 
 def stringToInt(message):
+    """Wrapper function for above two funtions to convert a string to a base 10 integer based on the ASCII values of its characters"""
     return bitListToInt(stringToBitList(message))
 
-def NumToString(message):
-    pass
+def binstringToBitList(binstring): 
+    bitList = []
+    for bit in binstring:
+        bitList.append(int(bit))
+    return bitList
+
+def padBits(bits, padding):
+    return [0] * (padding-len(bits)) + bits
+
+def bitsToString(paddedBitSeq):
+    charBuilder = ''
+    for segment in range(0,len(paddedBitSeq),8): # count by 8's because each padded bit sequence is 8 bits long
+        charBuilder = charBuilder + bitsToChar(paddedBitSeq[segment: segment+8]) # concatenate each new char onto the built up string
+    return charBuilder
+
+def bitsToChar(bitSeq):
+    """Convert each 8 bit length padded bit sequences back to a char based on its ASCII value"""
+    value = 0
+    for bit in bitSeq:
+        value = (value * 2) + bit # This for loop will determine the numeric value of the binary bit sequence input
+    return chr(value)
+
+def intToString(integer):
+    """Wrapper function for above four funtions to convert an integer of base 10 to a string based on the ASCII values of its characters"""
+    binary = bin(integer)[2:]
+    bitSeq = binstringToBitList(binary)
+    return bitsToString(padBits(bitSeq,(len(bitSeq)+(8-(len(bitSeq)%8)))))
 
 def modularInverse(a,mod):
     pass
@@ -41,7 +67,7 @@ def encrypt(message,n,totient,e,printKeys):
 
 def decrypt(encryptedMsg,publickey,privateKey):
     decryptedMsg = pow(int(encryptedMsg),int(privateKey),int(publicKey))
-    outputString = NumToString(decryptedMsg)
+    outputString = intToString(decryptedMsg)
     return outputString
 
 if __name__ == '__main__':
